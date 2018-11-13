333a2fe2faeec610edd9bd7a7956105a0487a255
JJ Merelo <jjmerelo@gmail.com>
2018-11-13 17:07:39 +0100

    Adding some components

---
+++ theme.js
@@ -0,0 +1,34 @@
+// example theme.js
+import { future } from 'mdx-deck/themes'
+import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
+import prismBash from 'react-syntax-highlighter/languages/prism/bash'
+const monospace = 'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace'
+const allerta = 'Allerta Stencil, sans-serif'
+const opensans = 'Open Sans, sans-serif'
+
+export default {
+	...future,
+    font: allerta, opensans,
+    heading: {
+	fontFamily: allerta,
+    },
+    h1: {
+	fontSize: '5em',
+	textTransform: 'none',
+	fontFamily: allerta,
+    },
+    h2: {
+	fontSize: '3em',
+	textTransform: 'none',
+	fontFamily: opensans,
+    },
+    prism: {
+	style: okaidia,
+	fontSize: '2em',
+	languages: {
+	    bash: prismBash
+	}
+    }
+    
+}
+
