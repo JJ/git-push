43e24aba53dab1864ae74cdad1de474c69fe3a85
JJ Merelo <jjmerelo@gmail.com>
2018-11-14 13:49:09 +0100

    Mejoras en el tema

--- theme.js
+++ theme.js
@@ -3,24 +3,21 @@ import { future } from 'mdx-deck/themes'
 import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
 import prismBash from 'react-syntax-highlighter/languages/prism/bash'
 const monospace = 'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace'
-const allerta = 'Allerta Stencil, sans-serif'
-const opensans = 'Open Sans, sans-serif'
+const h1font = "Oswald, sans-serif"
+const h2font = "'Open Sans', sans-serif"
 
 export default {
 	...future,
-    font: allerta, opensans,
-    heading: {
-	fontFamily: allerta,
-    },
+    font: h1font, h2font,
     h1: {
 	fontSize: '5em',
 	textTransform: 'none',
-	fontFamily: allerta,
+	fontFamily: h1font
     },
     h2: {
 	fontSize: '3em',
 	textTransform: 'none',
-	fontFamily: opensans,
+	fontFamily: h2font,
     },
     code: {
       	fontSize: '1.5em',
