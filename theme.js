d4e293a01046590be33069c25b3f367d35f50678
JJ Merelo <jjmerelo@gmail.com>
2018-11-15 09:41:02 +0100

    Añade slides y cambia formato

--- theme.js
+++ theme.js
@@ -3,14 +3,14 @@ import { future } from 'mdx-deck/themes'
 import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
 import prismBash from 'react-syntax-highlighter/languages/prism/bash'
 const monospace = 'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace'
-const h1font = "Oswald, sans-serif"
+const h1font = "Archivo, sans-serif"
 const h2font = "'Open Sans', sans-serif"
 
 export default {
 	...future,
     font: h1font, h2font,
     h1: {
-	fontSize: '5em',
+	fontSize: '4em',
 	textTransform: 'none',
 	fontFamily: h1font
     },
